0259e8c96bc211c0240beade95d2240f3bae0403
Robert Weis <gbobweis@gmail.com>
2022-12-15 19:32:08 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
@@ -15,7 +16,8 @@ export default function App({ Component, pageProps }: AppProps) {
   if(isSSR) return null;
 
   return (
-    <div>
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`} >
+      <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh] '>
       <Navbar />
       <div className='flex gap-6 md:gap-20'>
         <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
@@ -25,6 +27,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </div>
       </div>
-    </div>
+      </div>
+    </GoogleOAuthProvider>
   );
 }
